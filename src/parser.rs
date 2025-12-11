9bfae58453f7b1a91b98d8503c07685d23243f68
Kian <kianbehjati_84@outlook.com>
2025-12-11 13:45:12 +0330

    UserInput fix

--- src/parser.rs
+++ src/parser.rs
@@ -1,24 +1,40 @@
 use std::env;
 
+use crate::check;
+
 pub struct DjangoOptions {
     pub name:String,
     pub apps:String,
 }
 
-pub fn parser() -> DjangoOptions<>{
+pub fn parser() -> Option<DjangoOptions>{
     let args:Vec<_> = env::args().collect();
+
+    for mut arg in args.clone(){
+        arg = arg.to_lowercase();
+        if arg.contains("-h") {
+            return None;
+        }
+    }
     match args.len() {
         2 => {
-            return DjangoOptions{name : args[1].clone(),apps : String::from("")};
+            if args[1].len() > 25{
+                panic!("Django Project name is too long!!!")
+            }
+            return Some(DjangoOptions{name : args[1].clone(),apps : String::from("")});
            
         }
         3 => {
-            return DjangoOptions{name : args[1].clone(),apps : args[2].clone()};
+            if args[1].len() > 25{
+                panic!("Django Project name is too long!!!")
+            }
+            return Some(DjangoOptions{name : args[1].clone(),apps : args[2].clone()});
         }
         _ => {
-            return DjangoOptions{name : String::from("core"),apps : String::from("")};
+            return Some(DjangoOptions{name : String::from("core"),apps : String::from("")});
         }
         // needs more work
     }
+    
    
 }
