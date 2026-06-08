ec02e174227dce2aee5f46842425344291508e4c
Kian <kianbehjati_84@outlook.com>
2026-06-08 16:56:47 +0330

    Match starter with new logic     - stater takes new arg (path)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
+use std::io;
+
 mod starter;
 mod check;
 mod parser;
 fn main(){
-
-    let res = parser::parser();
-    match res {
-        Some(options) => starter::starter(options),
-        None => println!("Failed to parse arguments"),
-    }
+    unimplemented!();
 }
