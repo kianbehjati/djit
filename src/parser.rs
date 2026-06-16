7391c36502c284170c3f433609625c337f3e341c
Kian <kianbehjati_84@outlook.com>
2026-06-16 14:19:43 +0330

    rustfmt

--- src/parser.rs
+++ src/parser.rs
@@ -1,14 +1,14 @@
 use std::env;
 
 pub struct DjangoOptions {
-    pub name:String,
-    pub apps:String,
+    pub name: String,
+    pub apps: String,
 }
 
-pub fn parser() -> Option<DjangoOptions>{
-    let args:Vec<_> = env::args().collect();
+pub fn parser() -> Option<DjangoOptions> {
+    let args: Vec<_> = env::args().collect();
 
-    for mut arg in args.clone(){
+    for mut arg in args.clone() {
         arg = arg.to_lowercase();
         if arg.contains("-h") {
             return None;
@@ -16,23 +16,28 @@ pub fn parser() -> Option<DjangoOptions>{
     }
     match args.len() {
         2 => {
-            if args[1].len() > 25{
+            if args[1].len() > 25 {
                 panic!("Django Project name is too long!!!")
             }
-            return Some(DjangoOptions{name : args[1].clone(),apps : String::from("")});
-           
+            return Some(DjangoOptions {
+                name: args[1].clone(),
+                apps: String::from(""),
+            });
         }
         3 => {
-            if args[1].len() > 25{
+            if args[1].len() > 25 {
                 panic!("Django Project name is too long!!!")
             }
-            return Some(DjangoOptions{name : args[1].clone(),apps : args[2].clone()});
+            return Some(DjangoOptions {
+                name: args[1].clone(),
+                apps: args[2].clone(),
+            });
         }
         _ => {
-            return Some(DjangoOptions{name : String::from("core"),apps : String::from("")});
-        }
-        // needs more work
+            return Some(DjangoOptions {
+                name: String::from("core"),
+                apps: String::from(""),
+            });
+        } // needs more work
     }
-    
-   
 }
